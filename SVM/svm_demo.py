98e9cdc92db0628202f41a548fa475d3b04c5afe
yoBoyio <dimi044@hotmail.com>
2021-06-03 04:03:24 +0300

    oops

--- SVM/svm_demo.py
+++ SVM/svm_demo.py
@@ -53,65 +53,6 @@ def iris(plotting_results, live_plotting):
             plot_results(svc, X_test, y_test)
 
 
-def housing(plotting_results, live_plotting):
-    from sklearn.preprocessing import MinMaxScaler
-    # input_file = 'housing.data'
-    train_df = pd.read_csv(
-        'https://firebasestorage.googleapis.com/v0/b/bible-project-2365c.appspot.com/o/train.csv?alt=media&token=9c5d17c2-0589-43ea-b992-e7c2ad02d714', index_col='ID')
-    train_df.head()
-    test_df = pd.read_csv(
-        'https://firebasestorage.googleapis.com/v0/b/bible-project-2365c.appspot.com/o/test.csv?alt=media&token=99688b27-9fdb-4ac3-93b8-fa0e0f4d7540', index_col='ID')
-    test_df.head()
-
-    predictors = ['crim', 'zn', 'indus', 'chas', 'nox', 'rm',
-                  'age', 'dis', 'rad', 'tax', 'ptratio', 'black', 'lstat']
-    target = 'medv'
-
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    # Scale both the training inputs and outputs
-    scaled_train = scaler.fit_transform(train_df)
-    print("Note: median values were scaled by multiplying by {:.10f} and adding {:.6f}".format(
-        scaler.scale_[13], scaler.min_[13]))
-    multiplied_by = scaler.scale_[13]
-    added = scaler.min_[13]
-    scaled_train_df = pd.DataFrame(
-        scaled_train, columns=train_df.columns.values)
-
-    X_train = scaled_train_df.drop(target, axis=1).values
-    y_train = scaled_train_df[[target]].values
-
-    # from sklearn.preprocessing import LabelBinarizer
-    # y_dense = LabelBinarizer().fit_transform(y_train.astype(str))
-    # print(y_dense)
-
-    # y_train = y_dense
-
-    svc = create_SVC()
-
-    fit(svc, X_train, y_train, plot=live_plotting, isHousing=True)
-
-    test_error_rate = svc.score(X=X_train, y=y_train)
-    print("The mean squared error (MSE) for the test data set is: {}".format(
-        test_error_rate))
-
-    prediction = svc.predict(X_train)
-    y_0 = prediction[0]
-    print('Prediction with scaling - {}', format(y_0))
-    y_0 -= added
-    y_0 /= multiplied_by
-    print("Housing Price Prediction  - ${}".format(y_0))
-
-    Y_0 = y_train[0][0]
-    print('Ground truth with scaling - {}'.format(Y_0))
-    Y_0 -= added
-    Y_0 /= multiplied_by
-
-    print('Ground Truth Price - ${}'.format(Y_0))
-
-    if (plotting_results):
-        plot_results(svc, X_test, y_test)
-
-
 def live_plot(ax, X, y, y_pred, svc,  isIris=False, isHousing=False):
     # clear graphs
     ax[0][0].clear()
@@ -233,11 +174,9 @@ while(True):
     live_plotting_3d, plotting_results_3d = False, False
 
     file = input(
-        "Δώσε input file (a, b, c, d, ii_a, ii_b, iris, housing): ") or 'a'
+        "Δώσε input file (a, b, c, d, ii_a, ii_b, iris): ") or 'a'
     if (file == "iris"):
         iris(plotting_results, live_plotting)
-    elif(file == "housing"):
-        housing(live_plotting=live_plotting, plotting_results=plotting_results)
 
     else:
         input_file = 'data_package_%s.csv' % file
